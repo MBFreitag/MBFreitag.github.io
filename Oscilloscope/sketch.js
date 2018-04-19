cae4c85963f81d3c52691bc02ec298ad0e6a1a6e
Macklin Freitag <frema19@Macks-Laptop.local>
2018-04-19 13:44:09 -0700

    11th commit

--- Oscilloscope/sketch.js
+++ Oscilloscope/sketch.js
@@ -1,36 +1,92 @@
-// make some noise to float the ellipse
-// p5.AudioIn contains its own p5.Amplitude object,
-// so you can call getLevel on p5.AudioIn without
-// creating a p5.Amplitude.
+/**
+ * Display the average amount of energy (amplitude) across a range
+ * of frequencies using the p5.FFT class and its methods analyze()
+ * and getEnergy().
+ *
+ * This example divides the frequency spectrum into eight bands.
+ */
 
-var input;
-var analyzer;
+var soundFile;
+var fft;
+
+var description = 'loading';
+var p;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  soundFile = loadSound('../files/beat');
+}
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(1024, 400);
+  fill(255, 40, 255);
+  noStroke();
+  textAlign(CENTER);
 
-  // Create an Audio input
-  input = new p5.AudioIn();
+  fft = new p5.FFT();
 
-  // start the Audio Input
-  input.start();
-
-  // create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(input);
+  p = createP(description);
+  var p2 = createP('Description: Using getEnergy(low, high) to measure amplitude within a range of frequencies.');
 }
 
 function draw() {
-  background(200);
+  background(30,20,30);
+  updateDescription();
 
-  // Get the overall volume (between 0 and 1.0)
-  var vol = analyzer.getLevel();
-  fill(127);
-  stroke(0);
+  fft.analyze(); // analyze before calling fft.getEnergy()
 
-  // Draw an ellipse with height based on volume
-  var h = map(vol, 0, 1, height, 0);
-  ellipse(width/2, h - 25, 50, 50);
+  // Generate 8 bars to represent 8 different frequency ranges
+  for (var i = 0; i < 8; i++){
+    noStroke();
+    fill((i*30) % 100 + 50, 195, (i*25 + 50) % 255 )
+
+    // Each bar has a unique frequency range
+    var centerFreq = (pow(2,i)*125)/2;
+    var loFreq = (pow(2,i-1)*125)/2 + centerFreq/4;
+    var hiFreq = (centerFreq + centerFreq/2);
+
+    // get the average value in a frequency range
+    var freqValue = fft.getEnergy(loFreq, hiFreq - 1);
+
+    // Rectangle height represents the average value of this frequency range
+    var h = -height + map(freqValue, 0, 255, height, 0);
+    rect((i+1)*width/8 - width/8, height, width/8, h);
+
+    fill(255);
+    text( loFreq.toFixed(0) +' Hz - ' + hiFreq.toFixed(0)+' Hz', (i+1)*width/8 - width/8/2, 30);
+  }
+}
+
+function keyPressed() {
+  if (soundFile.isPlaying()){
+    soundFile.pause();
+  } else {
+    soundFile.loop();
+  }
+}
+
+// Change description text if the song is loading, playing or paused
+function updateDescription() {
+  if (!soundFile.isPlaying()) {
+    description = 'Paused...';
+    p.html(description);
+  }
+  else if (soundFile.isPlaying()){
+    description = 'Playing!';
+    p.html(description);
+  }
+  else {
+    for (var i = 0; i < frameCount%3; i++ ) {
+
+      // add periods to loading to create a fun loading bar effect
+      if (frameCount%4 == 0){
+        description += '.';
+      }
+      if (frameCount%25 == 0) {
+        description = 'loading';
+
+      }
+    }
+    p.html(description);
+  }
 }
