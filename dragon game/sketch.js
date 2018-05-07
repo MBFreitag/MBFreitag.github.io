8bb9740755d12279c1a8cc1855f45a83691ef8ef
Macklin Freitag <frema19@Macks-Laptop.local>
2018-05-07 16:26:01 -0700

    test two

--- dragon game/sketch.js
+++ dragon game/sketch.js
@@ -1,14 +1,14 @@
-var
-    cloud1;
+var cloud1,cloud2;
 function preload() {
     cloud1 = loadImage("cloud1.png");
 }
 
 function setup() {
     createCanvas(800, 600);
+    cloud2=cloud1;
 
 }
 function draw() {
     background(0);
-    image(cloud1,0,0);
+    image(cloud2,0,0);
 }
